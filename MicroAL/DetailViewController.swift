016d7b4e0a677e944830278dd937ac9f3330c343
Christopher Prince <chris@SpasticMuffin.biz>
2016-05-06 00:08:55 -0600

    First draft complete! Passing all tests.

--- MicroAL/DetailViewController.swift
+++ MicroAL/DetailViewController.swift
@@ -12,20 +12,127 @@ import MapKit
 class DetailViewController: UIViewController {
     // Set this before navigating to the view controller
     var serviceProvider:ServiceProvider?
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var address: UILabel!
-    @IBOutlet weak var reviews: UILabel!
-    @IBOutlet weak var grade: UILabel!
+    
+    var mapView = MKMapView()
+    var textInfoView = UIView()
+    var nameAndAddress = UIView()
+    var name = UILabel()
+    var address = UILabel()
+    var reviewsAndGrade = UIView()
+    var reviews = UILabel()
+    var grade = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(self.mapView)
+        self.view.addSubview(self.textInfoView)
+        
+        self.textInfoView.addSubview(self.nameAndAddress)
+        self.textInfoView.addSubview(self.reviewsAndGrade)
+        
+        self.nameAndAddress.addSubview(self.name)
+        self.nameAndAddress.addSubview(self.address)
+        
+        self.reviewsAndGrade.addSubview(self.reviews)
+        self.reviewsAndGrade.addSubview(self.grade)
+    }
+    
+    private let mainViewSpacing:CGFloat = 20
+    private let borderMargin:CGFloat = 20
+    
+    private func layoutViews() {
+        var width:CGFloat
+        var height:CGFloat
+        
+        // Getting a call to layoutViews (i.e., viewDidLayoutSubviews) on exiting the nav controller back to the master VC. Odd.
+        if self.navigationController == nil {
+            return
+        }
+        
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.size.height
+        let navStatusBarHeight = self.navigationController!.navigationBar.frameHeight + statusBarHeight
+        let viewControllerHeight = self.view.frameHeight - navStatusBarHeight
+        
+        if self.view.frameWidth > self.view.frameHeight {
+            // landscape
+            width = self.view.frameWidth/2.0
+            height = viewControllerHeight
+        }
+        else {
+            // portrait
+            width = self.view.frameWidth
+            height = viewControllerHeight/2.0
+        }
+        
+        width -= 2*self.borderMargin
+        height -= 2*self.borderMargin
+        
+        let size = CGSize(width: width, height: height)
+        self.mapView.frameSize = size
+        self.textInfoView.frameSize = size
+        
+        self.mapView.frameY = navStatusBarHeight + self.borderMargin
+        self.mapView.frameX = self.borderMargin
+        
+        if self.view.frameWidth > self.view.frameHeight {
+            // landscape
+            self.textInfoView.frameOrigin = CGPoint(x: self.mapView.frameMaxX + mainViewSpacing, y: navStatusBarHeight + self.borderMargin)
+        }
+        else {
+            // portrait
+            self.textInfoView.frameOrigin = CGPoint(x: self.borderMargin, y: self.mapView.frameMaxY + mainViewSpacing)
+        }
+        
+        // We've got enough space vertically, so use it if we need it-- if we're cutting off the name/address text horizontally.
+        self.name.numberOfLines = 1
+        self.address.numberOfLines = 1
+        self.name.sizeToFit()
+        self.address.sizeToFit()
+        
+        func useTwoLinesIfNeededFor(label label:UILabel) {
+            if label.frameWidth > width {
+                label.numberOfLines = 2
+                let size = label.sizeThatFits(CGSize(width: width, height: CGFloat(FLT_MAX)))
+                label.frameSize = size
+            }
+        }
+        
+        useTwoLinesIfNeededFor(label: self.name)
+        useTwoLinesIfNeededFor(label: self.address)
+        
+        self.address.frameY = self.name.frameMaxY
+        self.nameAndAddress.frameSize = CGSize(width: width, height: self.address.frameMaxY)
+        
+        self.reviewsAndGrade.frameWidth = width
+        self.reviewsAndGrade.frameHeight = self.reviews.frameHeight
+        self.grade.frameMaxX = self.reviewsAndGrade.frameWidth
+        
+        // Looks better, at least on iPhone to have this aligned with bottom
+        self.reviewsAndGrade.frameMaxY = height
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.layoutViews()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
         self.title = self.serviceProvider!.name
+        
+        self.name.font = UIFont.systemFontOfSize(30)
+        self.address.font = UIFont.systemFontOfSize(25)
+        
+        self.name.text = self.serviceProvider!.name
+        self.address.text = "\(self.serviceProvider!.city!), \(self.serviceProvider!.state!) \(self.serviceProvider!.postalCode!)"
+        self.reviews.text = "Reviews: \(self.serviceProvider!.reviewCount!)"
+        self.grade.text = "Grade: \(self.serviceProvider!.overallGrade!)"
+        
+        // These are small enough to always fit. Don't worry about changing their size with rotation or amount of text.
+        self.reviews.sizeToFit()
+        self.grade.sizeToFit()
         
         // Drop the pin
         let annotation = MKPointAnnotation()
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ https://github.com/crspybits/MicroAL.git
 
 2) SMCoreLib includes AFNetworking. Hence, I've not linked that in separately.
 
-3) This project targets iOS8 both because Apple recommends supporting one version back of the system and because the SMCoreLib uses UIAlertView's, and we'd get warnings otherwise :).
+3) There are several warnings regarding UIAlertView's. I need to update a library to using the new iOS9 UIAlertController's. I can't target below iOS9 because in some cases I'm using UIStackView's.
 
 4) I've added the following key/value into Info.plist because the URL uses HTTP not HTTPS. For a production app, for security purposes, Apple recommends dropping this key and using HTTPS.
 
@@ -17,3 +17,5 @@ https://github.com/crspybits/MicroAL.git
 5) While Core Data was not called for in the requirements, I've used it for several reasons: 1) I've got a standard (SMCoreLib) collection of methods to provide a data source for a table view using Core Data, and so this simplifies the problem of populating the table view, 2) Core Data provides some standard means of sorting data, which is needed in the problem, and 3) Core Data abstracts out some common data management problems. Because I don't specifically need the persistence aspect of Core Data, all Core Data objects are flushed at launch.
 
 6) From what I can tell, the linker warning "embedded dylibs/frameworks only run on iOS 8 or later" is an Xcode bug.
+
+7) I laid out the views in the detail view controller using code both to illustrate that I can do this and because the rotation was annoying with constraints/autolayout. It seemed simpler to do it in code.
